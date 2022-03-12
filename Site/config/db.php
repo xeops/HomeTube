9efeeccd11643a11360d936b88dcf2400423e28a
alexei <ageneralov@iiko.ru>
2022-03-12 15:38:28 +0300

    init

---
+++ Site/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'class' => 'yii\db\Connection',
+	'dsn' => 'pgsql:host=localhost;port=5432;dbname=video_tube',
+	'username' => 'postgres',
+	'password' => 'GpaWAT334AAFrFA',
+	'charset' => 'utf8'
+];
---
+++ Site/migrations/m220312_114457_video.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220312_114457_video
+ */
+class m220312_114457_video extends Migration
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp()
+	{
+		$this->createTable('video', [
+			'id' => $this->primaryKey(),
+			'title' => $this->string()->notNull(),
+			'thumbnail' => $this->string()->defaultValue('/assets/img/thumbnail.png'),
+			'duration' => $this->integer()->unsigned(),
+			'views' => $this->bigInteger()->unsigned()->defaultValue(0),
+			'added' => $this->dateTime()->defaultExpression('now()')
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown()
+	{
+		echo "m220312_114457_video cannot be reverted.\n";
+
+		return false;
+	}
+
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m220312_114457_video cannot be reverted.\n";
+
+		return false;
+	}
+	*/
+}
